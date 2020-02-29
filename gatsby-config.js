e746d2da7061d74e304c695ab5875e2b6807e5f4
ajz003 <ajz003@ucla.edu>
2020-02-28 22:57:45 -0800

    update manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,13 +20,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Anthony Zheng`,
+        short_name: `ajz003`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#171717`,
+        theme_color: `#13aff0`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/favicon.ico`, // This path is relative to the root of the site.
       },
     },
     `gatsby-plugin-sass`,
